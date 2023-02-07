e07340ef9da352afba1f07a132f67f6c91e9b9af
KHS <ewoking777@gmail.com>
2023-02-08 00:31:50 +0900

    A+B 수학 구현 사칙연산

---
+++ Test/Lv.1/No1000.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class No1000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int no_1 = scanner.nextInt();
+        int no_2 = scanner.nextInt();
+        int sum = no_1 + no_2;
+        System.out.println(sum);
+    }
+}
