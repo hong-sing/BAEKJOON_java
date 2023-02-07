2402c0340c0eb0340193c3421356bd336eaae05d
KHS <ewoking777@gmail.com>
2023-02-08 00:35:32 +0900

    A×B 수학 구현 사칙연산

---
+++ Test/Lv.1/No10998.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class No10998 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int answer = A * B;
+        System.out.println(answer);
+    }
+}
